0403114f54bb513ee4402333947789e9a9ea8349
Hevin <liuhw@jpush.cn>
2017-08-14 17:14:09 +0800

    Fixed updateMyAvatar

--- src/android/JsonUtils.java
+++ src/android/JsonUtils.java
@@ -6,7 +6,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
